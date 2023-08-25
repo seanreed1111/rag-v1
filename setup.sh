54b125f45ea78161467053d3e00761f55659739e
Sean Reed <seanreed1111@gmail.com>
2023-08-24 21:33:22 -0400

    fixes setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ sudo apt-get upgrade -y && \
 sudo apt install python3 -y && \
 sudo apt install python3-pip -y && \
 export HOME="/home/vmuser" && \
-sudo pip install -r requirements.txt && \
+export PATH="$HOME/.local/bin:$PATH" && \
+pip install -r requirements.txt && \
 
 # DEFAULT PORT for streamlit is 8501
-streamlit run app_v3.py
+streamlit run app.py
